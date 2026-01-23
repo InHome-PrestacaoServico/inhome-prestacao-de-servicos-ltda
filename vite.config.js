a697d864021960458631ff3ea10a49dd1c385f66
YanFellippe <yangomesbasilio@gmail.com>
2026-01-23 13:11:02 -0300

    Corrigir configuração do Vite para usar esbuild

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,23 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild', // Usar esbuild que já vem com Vite (não precisa instalar)
     rollupOptions: {
       input: {
         main: './index.html'
       },
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'css/[name][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
       }
-    }
+    },
+    assetsInlineLimit: 0,
+    cssCodeSplit: false,
   },
   server: {
     port: 3000,
